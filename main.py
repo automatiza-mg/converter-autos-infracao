9fad727c2670c3d4933f45985638c09e1f468035
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-05-10 13:53:51 -0300

    Get first column information

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pandas as pd
 
 df = pd.read_excel('so_table.xlsx', sheet_name='Table 1')
-import ipdb; ipdb.set_trace(context=10)
+
+def remove_new_line(text):
+    text_list = text.split(':')[-1].split('\n')
+    final_text = [item for item in text_list if item]
+    return ' '.join(final_text)
+
+new_df = {}
+for column in range(0, len(df.columns)):
+    content = df.iloc[:,0]
+    if df.iloc[:,0][0] == 'FICHA DE FISCALIZAÇÃO':
+        new_df['tipificacao_resumida'] = remove_new_line(content[1])
+        new_df['amparo_legal'] = remove_new_line(content[2])
+        new_df['tipificacao_enquadramento'] = remove_new_line(content[3])
+        new_df['gravidade'] = remove_new_line(content[4])
+        new_df['infrator'] = remove_new_line(content[5])
+        new_df['pontuacao'] = remove_new_line(content[6])
+        new_df['quando_autuar'] = remove_new_line(content[8])
+        import ipdb; ipdb.set_trace(context=10)
