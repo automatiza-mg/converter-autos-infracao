cdc597d4a9b778fd090ec2d4eca1f6573a420ece
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-05-14 17:38:39 -0300

    Adiciona lógica para buscar resto da tabela
    Até tabela 8
    Tabela completa
    
    Tabela 9 não buscando informações complementares

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from table_main_content import get_table_main_content
+from table_another_info import get_table_another_info
 from utils import (
     build_new_df_dict,
     remove_new_line_text,
@@ -17,19 +18,18 @@ def convert_all_content_to_dict(sheet_names):
         df = pd.read_excel('so_table.xlsx', sheet_name=sheet_name)
 
         if len(first_column:= df.iloc[:,0]) > 0 and first_column[0] == 'FICHA DE FISCALIZAÇÃO':
-            if sheet_name == 'Table 9':
+            if sheet_name == 'Table 10':
                 break
             indexes = [i for i, item in enumerate(first_column) if item == 'FICHA DE FISCALIZAÇÃO']
             get_table_main_content(df, new_df_dict, sheet_name, indexes)
         elif len(first_column[0]) > 0 and first_column[0] == 'Informações Complementares:':
-            new_df_dict['sheet'][-1] += f' / {sheet_name}'
-            new_df_dict['informacoes_complementares'].append(remove_new_line_text(first_column[1]))
+            get_table_another_info(df, new_df_dict, sheet_name)
     return new_df_dict
 
 if __name__ == '__main__':
     new_df_dict = convert_all_content_to_dict(sheet_names)
     for key, item in new_df_dict.items():
         print(key, ' - ', len(item))
-    # import ipdb; ipdb.set_trace(context=10)
+    import ipdb; ipdb.set_trace(context=10)
     new_df = pd.DataFrame(new_df_dict)
     new_df.to_csv('multas.csv')
---
+++ src/table_another_info.py
@@ -0,0 +1,25 @@
+# import ipdb; ipdb.set_trace(context=10)
+import pandas as pd
+from utils import (
+    remove_new_line_field,
+    remove_new_line_text,
+)
+
+def get_table_another_info(df, new_df_dict, sheet_name):
+
+    for column in range(0, column_len:= len(df.columns)):
+        print(f'Avaliando coluna {column + 1} de {column_len} da planilha {sheet_name}')
+        content = df.iloc[:,column]
+
+        if column == 0:
+            new_df_dict['sheet'][-1] += f' / {sheet_name}'
+            new_df_dict['informacoes_complementares'].append(remove_new_line_text(content[1]))
+        elif column == 0 and not content.name.startswith('Unnamed:'):
+            new_df_dict['quando_autuar'][-1] += f' {remove_new_line_text(content.name)}'
+        elif column == 1 and not content.name.startswith('Unnamed:'):
+            new_df_dict['quando_nao_autuar'][-1] += f' {remove_new_line_text(content.name)}'
+        elif column == 2 and not content.name.startswith('Unnamed:'):
+            new_df_dict['definicoes_procedimentos'][-1] += f' {remove_new_line_text(content.name)}'
+        elif column == 2 and not content.name.startswith('Unnamed:'):
+            new_df_dict['exemplos_campo_observacoes_ait'][-1] += f' {remove_new_line_text(content.name)}'
+    return None
